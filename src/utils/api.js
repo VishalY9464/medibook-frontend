2d7dd1f2a2252b5be0a6b08aa0202d7800c2a773
VishalY9464 <yaddavvishal9464@gmail.com>
2026-04-20 00:09:55 +0530

    MediBook frontend initial commit

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@
 
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080';
+const BASE_URL = 'https://paradox-landscape-sensitive.ngrok-free.dev';
 
 const api = axios.create({
   baseURL: BASE_URL,
